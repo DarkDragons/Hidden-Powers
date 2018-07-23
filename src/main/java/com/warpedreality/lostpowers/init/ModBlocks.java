aba28d2cc825ab97b6acb6ff9e2fbf59114f3c98
DarkDragons <pfaff.michael.j@gmail.com>
2018-07-23 08:41:33 -0400

    Obsidian Material is now Glass

--- src/main/java/com/warpedreality/lostpowers/init/ModBlocks.java
+++ src/main/java/com/warpedreality/lostpowers/init/ModBlocks.java
@@ -13,6 +13,6 @@ import java.util.List;
 public class ModBlocks {
     public static final List<Block> BLOCKS = new ArrayList<Block>();
 
-    public static final Block OBSIDIAN_BLOCK_1 = new BlockBase("obsidian_block_1", Material.ROCK, Main.tabLostPowers, SoundType.STONE,55, 6250, "pickaxe", 4);
-    public static final Block OBSIDIAN_BLOCK_2 = new BlockBase("obsidian_block_2", Material.ROCK, Main.tabLostPowers, SoundType.STONE,60, 6500, "pickaxe", 5);
+    public static final Block OBSIDIAN_BLOCK_1 = new BlockBase("obsidian_block_1", Material.GLASS, Main.tabLostPowers, SoundType.STONE,55, 6250, "pickaxe", 4);
+    public static final Block OBSIDIAN_BLOCK_2 = new BlockBase("obsidian_block_2", Material.GLASS, Main.tabLostPowers, SoundType.STONE,60, 6500, "pickaxe", 5);
 }
